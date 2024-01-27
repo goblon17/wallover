74250cdd45d35ac6ce66474674d939222449cdad
Pojemnik <andriej194@gmail.com>
2024-01-27 19:33:31 +0100

    Fix target position in jump

--- Assets/Scripts/Player/PlayerJumper.cs
+++ Assets/Scripts/Player/PlayerJumper.cs
@@ -32,6 +32,7 @@ public class PlayerJumper : MonoBehaviour
 		isJumping = true;
 		targetPositions = targetPosition;
 		currentJumpTime = 0;
+		Debug.Log(targetPositions.Root.Item1);
 		//Turn off animation
 	}
 
