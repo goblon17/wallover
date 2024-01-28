e48760eeba8a1262992aff7270d2b1e59de14950
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2024-01-28 13:54:19 +0100

    fix movie maker ad sound player

--- Assets/Scripts/GameControl/LevelMusic.cs
+++ Assets/Scripts/GameControl/LevelMusic.cs
@@ -9,16 +9,29 @@ public class LevelMusic : MonoBehaviour
     [SerializeField]
     private List<AudioClip> musics;
 
+    private bool isPlayingAd = false;
+
     private void Start()
     {
         WallManager.Instance.WallStartedEvent += OnWallStarted;
-        AdPlayer.Instance.AdStartedEvent += () => audioSource.Stop();
-        AdPlayer.Instance.AdEndedEvent += () => audioSource.Play();
+        AdPlayer.Instance.AdStartedEvent += () =>
+        {
+            audioSource.Stop();
+            isPlayingAd = true;
+        };
+        AdPlayer.Instance.AdEndedEvent += () =>
+        {
+            audioSource.Play();
+            isPlayingAd = false;
+        };
     }
 
     private void OnWallStarted()
     {
         audioSource.clip = musics.GetRandomElement();
-        audioSource.Play();
+        if (!isPlayingAd)
+        {
+            audioSource.Play();
+        }
     }
 }
